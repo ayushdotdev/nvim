9b0961af6c0e60828e2a60c08088690d0e180b3d
Ayush Kumar <ayush438500@gmail.com>
2026-01-20 19:38:23 +0530

    Lazyvim setup

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup{
+      color_overrides = {
+        all = {
+            text = "#ffffff",
+          },
+        mocha = {
+          base = "#000000",
+          crust = "#000000",
+          mantle = "#000000",
+        },
+      }
+      }
+  end,
+
+  }
