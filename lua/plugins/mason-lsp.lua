6eb03dbe44476bd2ade3e41d2bd4cc7b0d3a4f3b
Ayush Kumar <ayush438500@gmail.com>
2026-01-31 18:11:22 +0530

    Turned off the annoying swap files

--- lua/plugins/mason-lsp.lua
+++ lua/plugins/mason-lsp.lua
@@ -7,7 +7,7 @@ return {
   },
   config = function()
     require("mason-lspconfig").setup {
-      ensure_installed = { "pyright", "gopls", "html", "emmet_ls" },
+      ensure_installed = { "pyright", "gopls", "html", "somesass_ls" },
       automatic_enable = true,
     }
   end
