ebe046f9e92e66ba809f964b8e01b4abe81e35c3
Ayush Kumar <ayush438500@gmail.com>
2026-05-10 21:59:31 +0530

    add colorscheme

--- init.lua
+++ init.lua
@@ -10,3 +10,19 @@ vim.keymap.set('n', '<leader>o', ":update<CR> :source<CR>")
 vim.keymap.set('n', '<leader>w', ":write<CR>")
 vim.keymap.set('n', '<leader>q', ":quit<CR>")
 
+
+vim.pack.add({
+	{ src = "https://github.com/catppuccin/nvim" , name = "catppuccin" }
+})
+
+
+require("catppuccin").setup {
+	color_overrides = {
+		mocha = {
+			base = "#000000",
+			mantle = "#000000",
+			crust = "#000000",
+		}
+	}
+}
+vim.cmd("colorscheme catppuccin-mocha")
