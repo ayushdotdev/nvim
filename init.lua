f9a274e10541f85c4f041f9787d0bb8b570b7ea9
Ayush Kumar <ayush438500@gmail.com>
2026-05-10 21:44:44 +0530

    complete basic settings

--- init.lua
+++ init.lua
@@ -1,4 +1,12 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.fillchars = { eob = " " }
 
 vim.opt.swapfile = false
+vim.opt.wrap = false
+
+vim.g.mapleader = " "
+vim.keymap.set('n', '<leader>o', ":update<CR> :source<CR>")
+vim.keymap.set('n', '<leader>w', ":write<CR>")
+vim.keymap.set('n', '<leader>q', ":quit<CR>")
+
