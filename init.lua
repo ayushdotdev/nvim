9a681c57b0a14d56693bc3c9fa460695a6036bb4
Ayush Kumar <ayush438500@gmail.com>
2026-06-11 22:41:17 +0530

    Add indent-blankline.nvim plugin

--- init.lua
+++ init.lua
@@ -37,6 +37,7 @@ vim.pack.add({
     { src = "https://github.com/saghen/blink.lib" },
     { src = "https://github.com/saghen/blink.cmp" },
     { src = "https://github.com/sudoscrawl/tokyo-night-dark.nvim" },
+    { src = "https://github.com/lukas-reineke/indent-blankline.nvim"},
 })
 
 
@@ -120,3 +121,29 @@ require("lualine").setup()
 
 vim.cmd("colorscheme catppuccin-mocha")
 vim.lsp.buf.hover()
+
+
+local highlight = {
+    "RainbowRed",
+    "RainbowYellow",
+    "RainbowBlue",
+    "RainbowOrange",
+    "RainbowGreen",
+    "RainbowViolet",
+    "RainbowCyan",
+}
+
+local hooks = require "ibl.hooks"
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+    vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+    vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+    vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+    vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+    vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+    vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+    vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+end)
+
+require("ibl").setup { indent = { highlight = highlight } }
